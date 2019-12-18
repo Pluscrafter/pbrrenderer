778b31c3ffc6b89a44272000ebeeff9d570fe8f1
D3PSi <d3psigames@gmail.com>
2019-12-18 19:58:09 +0100

    Start shader abstraction

---
+++ pbrrenderer/pbr/core/PBRShaderInterface.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file PBRShaderInterface.hpp
+ * @brief Declares a wrapper for shaders
+ * @author D3PSI
+ */
+#ifndef PBR_SHADER_INTERFACE_HPP
+#define PBR_SHADER_INTERFACE_HPP
+
+namespace pbr {
+
+    namespace core {
+
+        class PBRShaderInterface
+        {
+        public:
+
+            /**
+             * Default constructor
+             */
+            PBRShaderInterface(void) = default;
+
+            /**
+             * Default destructor
+             */
+            ~PBRShaderInterface(void) = default;
+
+        private:
+
+        };
+
+    }
+
+}
+
+#endif      // PBR_SHADER_INTERFACE_HPP
---
+++ pbrrenderer/pbr/core/PBRShaderProgramHandler.hpp
@@ -0,0 +1,35 @@
+/**
+ * @file PBRShaderProgramHandler.hpp
+ * @brief Declares a wrapper for shader programs
+ * @author D3PSI
+ */
+#ifndef PBR_SHADER_PROGRAM_HANDLER_HPP
+#define PBR_SHADER_PROGRAM_HANDLER_HPP
+
+namespace pbr {
+
+    namespace core {
+
+        class PBRShaderProgramHandler 
+        {
+        public:
+
+            /**
+             * Default constructor
+             */
+            PBRShaderProgramHandler(void) = default;
+
+            /**
+             * Default destructor
+             */
+            ~PBRShaderProgramHandler(void) = default;
+
+        private:
+
+        };
+
+    }
+
+}
+
+#endif      // PBR_SHADER_PROGRAM_HANDLER_HPP
